61bd6714fa3422b6a39e302464e6b1edd0db28f8
AkshayKD26 <akshayd1994@gmail.com>
2020-06-18 13:09:06 +0530

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React,{Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import { DISHES } from './shared/dishes';
-import Menu from './components/MenuComponent';
-import './App.css';
+import Main from './components/MainComponent';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
-render(){
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/"> Akshay Dwivedi Restron </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />>
-    </div>
-  );
-}
 }
 export default App ;
